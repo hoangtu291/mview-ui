cb6666af31e49e56c3d130cb6cf415404a062494
lhtu.wrk <lhtu.wrk@gmail.com>
2025-01-28 01:12:44 +0700

    update SEO

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,4 +9,54 @@
 <script lang="ts" setup>
 import Footer from '~/components/Footer.vue'
 import Header from '~/components/Header.vue'
+
+useHead({
+  title: 'Trang Chủ | mViews',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'mViews - cung cấp các dịch vụ sáng tạo nội dung, đào tạo đồ họa, phát triển game và nhiều dịch vụ khác.',
+    },
+    {
+      name: 'keywords',
+      content:
+        'sáng tạo nội dung, đồ họa, hoạt hình, phát triển game, đào tạo TikTok, mview youtube,mview facebook, tiktok, lập trình',
+    },
+    {
+      property: 'og:title',
+      content: 'Trang Chủ | Tên Công Ty',
+    },
+    {
+      property: 'og:description',
+      content:
+        'Chúng tôi cung cấp các dịch vụ sáng tạo nội dung, đào tạo đồ họa, phát triển game và nhiều dịch vụ khác.',
+    },
+    {
+      property: 'og:image',
+      content: '/assets/logo-label.jpeg',
+    },
+    {
+      property: 'og:url',
+      content: 'https://yourwebsite.com',
+    },
+    {
+      name: 'twitter:card',
+      content: '/assets/logo-label.jpeg',
+    },
+    {
+      name: 'twitter:title',
+      content: 'Trang Chủ | mViews',
+    },
+    {
+      name: 'twitter:description',
+      content:
+        'MViews - Chúng tôi cung cấp các dịch vụ sáng tạo nội dung, đào tạo đồ họa, phát triển game và nhiều dịch vụ khác.',
+    },
+    {
+      name: 'twitter:image',
+      content: '/assets/logo-label.jpeg',
+    },
+  ],
+})
 </script>
